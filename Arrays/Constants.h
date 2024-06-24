2c4aee3df6de9a87fcac2eb244d9eefc6f0b4bd3
hawri <hawri@DESKTOP-BMM1386>
2024-06-24 12:15:47 +0300

    Separation Done

---
+++ Arrays/Constants.h
@@ -0,0 +1,6 @@
+#pragma once
+#define tab "\t"
+#define delimiter "\n---------------------------------\n"
+
+const int ROWS = 10;
+const int COLS = 10;
---
+++ Arrays/Sort.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"Constants.h"
+#include"stdafx.h"
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(char arr[], const int n);
+void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
